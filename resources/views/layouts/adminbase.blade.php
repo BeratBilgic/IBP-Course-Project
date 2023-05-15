25aa787a8dc98fcb42a8bd756f6242f8ed0cb288
Berat Bilgic <bertbilgc@gmail.com>
2023-05-15 10:57:48 +0300

    first commit

---
+++ resources/views/layouts/adminbase.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>@yield("title")</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>AdminLTE 3 | Blank Page</title>
+
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{asset('assets')}}/plugins/fontawesome-free/css/all.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{asset('assets')}}/dist/css/adminlte.min.css">
+    @yield("head")
+</head>
+
+<body>
+@include("admin.header")
+
+@section('sidebar')
+    @include("admin.sidebar")
+@show
+
+@yield('content')
+
+<@include("admin.footer")
+@yield('foot')
+</body>
+</html>
