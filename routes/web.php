9050ddb7dbeb4b4dc18a6625db6ca94b8a6443be
Berat Bilgic <bertbilgc@gmail.com>
2023-05-15 22:08:15 +0300

    create post and get all

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AdminPanel\HomeController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -28,6 +29,21 @@ Route::middleware([
     })->name('dashboard');
 });
 
-// ******************** ADMIN PANEL  ROUTES *************************
-Route::get('/admin',[HomeController::class,'index'])->name('index');
+// ******************** ADMIN PANEL ROUTES *************************
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::get('/',[HomeController::class,'index']);
+
+// ******************** ADMIN PRODUCT ROUTES *************************
+    Route::prefix('product')->controller(ProductController::class)->name('product.')->group(function () {
+        Route::get('/','index')->name('index');
+        Route::get('/create', 'create')->name('create');
+        Route::post('/store', 'store')->name('store');
+        Route::get('/edit/{id}','edit')->name('edit');
+        Route::post('/update/{id}','update')->name('update');
+        Route::get('/destroy/{id}','destroy')->name('destroy');
+        Route::get('/show/{id}','show')->name('show');
+    });
+});
+
+
 
